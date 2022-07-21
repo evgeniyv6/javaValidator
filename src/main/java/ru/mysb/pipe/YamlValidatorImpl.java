22836af04596473f203492c9e10a14952078ba4f
Yevgeniy <evgeniyv6@gmail.com>
2022-07-21 22:01:21 +0300

    init java repo

---
+++ src/main/java/ru/mysb/pipe/YamlValidatorImpl.java
@@ -0,0 +1,13 @@
+package ru.mysb.pipe;
+
+import ru.mysb.pipe.validators.InterfaceYamlValidator;
+
+public class YamlValidatorImpl implements InterfaceYamlValidator {
+
+    public void FooBar() {}
+
+    public static void main(String[] args) {
+        // impl
+        System.out.println("Hi foo bar java!");
+    }
+}
---
+++ src/main/java/ru/mysb/pipe/validators/InterfaceYamlValidator.java
@@ -0,0 +1,5 @@
+package ru.mysb.pipe.validators;
+
+public interface InterfaceYamlValidator {
+    public void FooBar();
+}
